a73ae2f31fe20929006015defc7588d7121f23e9
Setsenbileg Enkhbat <setsee@sets.local>
2025-01-07 22:35:37 +0100

    Edited pages

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -13,14 +13,14 @@ const routes = {
     '/':        true,
     '/about':   true,
     '/work':    true,
-    '/blog':    true,
+    '/blog':    false,
     '/gallery': true,
 }
 
 // Enable password protection on selected routes
 // Set password in pages/api/authenticate.ts
 const protectedRoutes = {
-    '/work/automate-design-handovers-with-a-figma-to-code-pipeline': true
+    '/work': true
 }
 
 const effects = {
